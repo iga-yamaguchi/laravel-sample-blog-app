3e2d5ee19dedd08cfae190208997338c45f64a34
iga-yamaguchi <iganin2@gmail.com>
2017-08-13 11:00:20 -0400

    WIP: Consider using mix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,8 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Sample Blog App</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <script src="/js/app.js"></script>
 </head>
 <body>
 <h1>Laravel Sample Blog App</h1>
