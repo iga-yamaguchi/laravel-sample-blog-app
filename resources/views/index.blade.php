71b316b9867b7fe114bd482ff76c62ccb25b78bb
iga-yamaguchi <iganin2@gmail.com>
2017-08-14 12:36:10 -0400

    Add bootstrap html from official bootstrap sample html.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,61 @@
     <script src="/js/app.js"></script>
 </head>
 <body>
-<h1>Laravel Sample Blog App</h1>
+<header>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Laravel Sample Blog App</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                    <li><a href="#">Link</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                           aria-expanded="false">Dropdown <span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">One more separated link</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
+</header>
+<main>
+    <div class="row">
+        @for($i = 0; $i < 20; $i++ )
+            <article class="col-sm-4">
+                <section>
+                    <h2>Title{{ $i }}</h2>
+                    <div>img...</div>
+                    <div>
+                        outline{{ $i }}
+                    </div>
+                </section>
+            </article>
+        @endfor
+    </div>
+</main>
+<footer>
+    <div class="panel panel-default">
+        <div class="panel-footer">copyright</div>
+    </div>
+</footer>
 </body>
 </html>
