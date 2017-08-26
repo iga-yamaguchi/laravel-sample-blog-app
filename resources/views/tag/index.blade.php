f8e3c170f0427c68d8ff01662be857abb992dd0a
iga-yamaguchi <iganin2@gmail.com>
2017-08-27 06:46:27 +0900

    Add delete method parameter

--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -8,7 +8,7 @@
                         <a href="{{ route('tag.show', $tag) }}"><h3>{{ $tag->name }}</h3></a>
                         <p><a href="{{ route('tag.show', $tag) }}">article counts : {{ $tag->articles->count() }}</a></p>
                         <a href="{{ route('tag.edit', $tag) }}" type="button" class="btn btn-primary">Edit</a>
-                        {{ Form::model($tag, ['class' => 'form-inline btn', 'route' => ['tag.destroy', $tag->id]]) }}
+                        {{ Form::model($tag, ['route' => ['tag.destroy', $tag->id], 'method' => 'DELETE', 'class' => 'form-inline btn']) }}
                         <div class="form-group">
                             {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
                         </div>
