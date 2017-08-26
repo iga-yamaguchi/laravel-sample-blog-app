6325157cf11b973a02331a7efda73983e1f7baa0
iga-yamaguchi <iganin2@gmail.com>
2017-08-26 15:51:20 +0900

    Add form helper and Create temporary tag create page

--- resources/views/tag/create.blade.php
+++ resources/views/tag/create.blade.php
@@ -1,5 +1,11 @@
 @extends('one-column')
 
-@section('content')
-    <p>Create Tag</p>
+@section('main')
+    {{ Form::open(['route' => 'tag.store']) }}
+    <div class="form-group">
+        {{ Form::Label('name', 'Tag name') }}
+        {{ Form::text('name', null, ['class' => 'form-control']) }}
+    </div>
+    {{ Form::submit('Submit', ['class' => 'btn btn-default']) }}
+    {{ Form::close() }}
 @endsection
