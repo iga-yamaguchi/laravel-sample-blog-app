72e69827e46c2106aa714e13e227cb9766256a00
iga-yamaguchi <iganin2@gmail.com>
2017-08-26 15:28:27 +0900

    Fix bootstrap and js error

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Laravel Sample Blog App</title>
     <link rel="stylesheet" type="text/css" href="/css/app.css">
-    <script src="/js/app.js"></script>
+    <script src="/js/bootstrap.js"></script>
 </head>
 <body>
 <header>
