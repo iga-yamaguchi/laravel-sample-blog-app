3e15fb8ce8aec35867183bcc6c373c1eaf5a2550
iga-yamaguchi <iganin2@gmail.com>
2017-09-03 12:42:21 +0900

    Add create test

--- tests/Feature/Controllers/ArticleControllerTest.php
+++ tests/Feature/Controllers/ArticleControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Controllers;
 
 use App\Article;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -15,10 +16,21 @@ class ArticleControllerTest extends TestCase
     public function setUp()
     {
         parent::setUp();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        DB::table('articles')->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
         $this->articles = factory(Article::class, 10)->create();
     }
 
-    public function testDeleteFromView()
+    public function testCreate()
+    {
+        $articles = factory(Article::class, 1)->make();
+
+        $this->post('article', $articles[0]->toArray())
+            ->assertStatus(200);
+        $this->assertDatabaseHas('articles', $articles[0]->toArray());
+    }
+
     public function testDelete()
     {
         $id = $this->articles[0]->id;
