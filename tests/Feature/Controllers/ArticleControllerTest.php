bd1a265b3d758628f0d1000d83a334acc207230f
iga-yamaguchi <iganin2@gmail.com>
2017-09-03 12:28:24 +0900

    Add delete controller test

---
+++ tests/Feature/Controllers/ArticleControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Controllers;
+
+use App\Article;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ArticleControllerTest extends TestCase
+{
+    private $articles;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->articles = factory(Article::class, 10)->create();
+    }
+
+    public function testDeleteFromView()
+    {
+        $id = $this->articles[0]->id;
+        $this->delete('article/' . $id)
+            ->assertStatus(200);
+        $this->assertDatabaseHas('articles', ['id' => $id])
+            ->assertDatabaseMissing('articles', ['id' => $id, 'deleted_at' => null]);
+    }
+}
