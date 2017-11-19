e2c1ba9b22d0e607430074f77b8e9ff96a9e00d8
iga-yamaguchi <iganin2@gmail.com>
2017-11-19 17:26:55 +0900

    Add comment

--- tests/TestUtils/TestRelation.php
+++ tests/TestUtils/TestRelation.php
@@ -3,20 +3,37 @@
 namespace Tests\TestUtils;
 
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\AssignOp\Mod;
+
 trait TestRelation
 {
     abstract public function assertEquals($expected, $actual, $message = '', $delta = 0, $maxDepth = 10, $canonicalize = false, $ignoreCase = false);
 
+    /**
+     * Assert a model relation.
+     *
+     * @param string $sourceClassName
+     * @param string $relationClassName
+     * @param string $relationAttributeName
+     */
     public function assertRelation(string $sourceClassName, string $relationClassName, string $relationAttributeName)
     {
+        /** @var Collection $relationTargets */
         $relationTargets = factory($relationClassName, 3)->create();
-        $source          = factory($sourceClassName)->create();
+        /** @var Model $source */
+        $source = factory($sourceClassName)->create();
 
         $ids = $relationTargets->pluck('id');
         $source->$relationAttributeName()->attach($ids);
 
         $this->assertEquals($relationTargets->count(), $source->$relationAttributeName->count());
 
+        /**
+         * @var int   $key
+         * @var Model $relation
+         */
         foreach ($source->$relationAttributeName as $key => $relation) {
             $args = $relationTargets[$key]->toArray();
 
